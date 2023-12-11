5a37284b7bd538ee6d34d9e9e86f2f1634a3dcaa
TomMirfin <t.a.mirfin@gmail.com>
2023-12-11 12:14:39 +0000

    buttons added and styling updated

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,19 +1,22 @@
+import Button from "@mui/material/Button";
 export default function ArticleCard({ article }) {
-  console.log(article);
   return (
     <div className="article-card">
       <div>
-        <p> Title </p>
-        <p>{article.title}</p>
+        <p className="card-title"> Title </p>
+        <p className="card-sub-title">{article.title}</p>
       </div>
       <div>
-        <p>Author: </p>
-        <p> {article.author}</p>
+        <p className="card-title">Author: </p>
+        <p className="card-sub-title"> {article.author}</p>
       </div>
       <img src={article.article_img_url} alt="" />
       <div>
-        <p>Topic</p>
-        <p>{article.topic}</p>
+        <p className="card-title"> Topic</p>
+        <p className="card-sub-title">{article.topic}</p>
+        <Button variant="contained" style={{ marginBottom: "20px" }}>
+          View Article
+        </Button>
       </div>
     </div>
   );
